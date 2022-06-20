72e38a9a321e690db40c1758097475d5eef85a45
anarosaa <silvaanarosa103@gmail.com>
2022-06-20 18:03:32 -0300

    desafio4

---
+++ script.js
@@ -0,0 +1,31 @@
+//1 - crie uma função que exiba uma mensagem no console
+function hellofriend(){
+    console.log('Hello friend!');
+}
+hellofriend();
+
+
+//2 - crie uma função que receba o seu nome como (parâmetro) e exiba no console
+function hipeople(name) {
+    console.log(`Olá ${name}`);
+  }
+  hipeople("Ana Rosa");
+
+
+ //3 - crie uma função que receba nome, idade, e um estilo musical preferido (parâmetros) e exiba no console 
+function information(nome, idade, estilomusical) {
+    console.log(`O meu nome é ${nome}, Minha idade é ${idade} e meu estilo musical é ${estilomusical}`);
+}
+information("Ana Rosa","16 anos","internacional.");
+
+
+//4 - crie uma função que receba um filme, uma música (parâmetros) e exiba no console
+function informationTwo(filme, musica){
+    console.log(`Um filme: ${filme}, Uma musica: ${musica}`);
+}
+informationTwo("The Conjuring", "Childish Gambino - Me and Your Mama.");
+
+
+ 
+
+
